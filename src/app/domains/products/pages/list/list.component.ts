810bfb4026724fb62893e3f45b846125471df340
Benjamin Ponce <benjamin.ponce@masuno.pe>
2025-01-15 09:56:46 -0500

    mostrando categorias

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -6,6 +6,8 @@ import { Product } from '@app/domains/shared/models/product.model';
 import { HeaderComponent } from '@app/domains/shared/components/header/header.component';
 import { CartService } from '@app/domains/shared/services/cart.service';
 import { ProductService } from '@app/domains/shared/services/product.service';
+import { CategoriesService } from '@app/domains/shared/services/categories.service';
+import { Category } from '@app/domains/shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -17,22 +19,16 @@ import { ProductService } from '@app/domains/shared/services/product.service';
 export class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   productsCart = signal<Product[]>([]);
 
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoriesService = inject(CategoriesService);
 
   ngOnInit() {
-    this.productService.getProduct().subscribe({
-      next: (products) => {
-        console.log('Products fetched: ', products);
-        this.products.set(products);
-      },
-      error: (error) => {
-        console.error('Error fetching products: ', error);
-      }
-    })
-    ;
+    this.getProducts();
+    this.getCategories();
   }
 
   cart = this.cartService.cart;
@@ -51,4 +47,29 @@ export class ListComponent {
   addToCart(product: Product) {
     this.productsInCart.push(product);
   }
+
+  private getProducts() {
+    this.productService.getProduct().subscribe({
+      next: (products) => {
+        console.log('Products fetched: ', products);
+        this.products.set(products);
+      },
+      error: (error) => {
+        console.error('Error fetching products: ', error);
+      }
+    });
+  }
+
+  private getCategories() {
+    this.categoriesService.getAll().subscribe({
+      next: (categories) => {
+        console.log('Categories fetched: ', categories);
+        this.categories.set(categories);
+      },
+      error: (error) => {
+        console.error('Error fetching categories: ', error);
+      }
+    });
+  }
+
 }
