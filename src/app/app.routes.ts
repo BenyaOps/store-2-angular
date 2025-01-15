f20ed4d43a4bd3815c17705fb47142520e675195
Benjamin Ponce <benjamin.ponce@masuno.pe>
2025-01-15 15:55:34 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 //import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 import { LayoutComponent } from './domains/shared/components/layout/layout.component';
+import { ProductDetailComponent } from './domains/products/pages/product-detail/product-detail.component';
 //import { ProductDetailComponent } from './domains/products/pages/product-detail/product-detail.component';
 
 export const routes: Routes = [
@@ -23,8 +24,8 @@ export const routes: Routes = [
       },
       {
         path: 'products/:id',
-        //component: ProductDetailComponent,
-        loadComponent: () => import('./domains/products/pages/product-detail/product-detail.component').then((m) => m.ProductDetailComponent),
+        component: ProductDetailComponent,
+        //loadComponent: () => import('./domains/products/pages/product-detail/product-detail.component').then((m) => m.ProductDetailComponent),
       }
     ],
   },
