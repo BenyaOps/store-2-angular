a417771a55c2e4cf9b96360d882e57907d86a873
Benjamin Ponce <benjamin.ponce@masuno.pe>
2025-01-17 13:45:01 -0500

    cambio

--- src/app/domains/shared/directives/highlight.directive.ts
+++ src/app/domains/shared/directives/highlight.directive.ts
@@ -1,17 +1,26 @@
-import { Directive, inject, ElementRef } from '@angular/core';
+import { Directive, inject, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 
 export class HighlightDirective {
+  constructor(private el: ElementRef) {
+    this.el.nativeElement.style.color = 'yellow';
+   }
+  //element = inject(ElementRef);
 
-  element = inject(ElementRef);
-
-  constructor() { }
-
-  ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = 'yellow';
+  @HostListener('mouseenter') onMouseEnter() {
+    this.el.nativeElement.style.color = 'red';
   }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.color = 'green';
+  }
+
+
+
+  //ngOnInit() {
+  //  this.element.nativeElement.style.backgroundColor = 'yellow';
+  //}
 
 }
